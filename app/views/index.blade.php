74338ab7b5358cb7e06e20fa3cc264eb27ee4f1b
Tom Eastwood <teastwood@vicimus.com>
2013-11-11 21:59:58 -0500

    just getting things started, kinda

---
+++ app/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('template')
+
+@section('content')
+	<p>This is some content for the index.</p>
+@stop
---
+++ app/views/template.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+
+<body>
+    <div style="width: 620px;">
+        <h1>Print Project</h1>
+
+        @yield('content')
+
+    </div>
+</body>
+
+</html>
